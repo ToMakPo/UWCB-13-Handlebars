1f8f356d6dac93c243b6a5e814f7d990a25de47b
Makai Post <postmakai@gmail.com>
2021-01-22 22:02:17 -0800

    created config file

---
+++ config/orm.js
@@ -0,0 +1,15 @@
+var connection = require("../config/connection.js")
+
+const orm = {
+    selectAll: function() {
+        const query = 'SELECT * FROM burgers'
+    },
+    insertOne: function(burgerName) {
+        const query = `INSERT INTO burgers (burger_name) VALUE ('${burgerName}')`
+    },
+    updateOne: function(id, devoured=true) {
+        const query = `UPDATE burgers SET devoured = ${devoured} WHERE id = ${id}`
+    },
+}
+
+module.exports = orm
