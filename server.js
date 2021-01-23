d2093ebb918c32491ef7375fa15641d63eb829d5
Makai Post <postmakai@gmail.com>
2021-01-23 00:15:36 -0800

    created server

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
-var express = require("express");
+const express = require('express')
 
-var PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080
 
-var app = express();
-app.use(express.static("public"));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+const app = express()
 
-var exphbs = require("express-handlebars");
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
-var routes = require("./controllers/catsController.js");
+app.use(express.static('public'))
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+const exphbs = require('express-handlebars')
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+const routes = require('./controllers/burgers_controller')
+app.use(routes)
+
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT)
+})
